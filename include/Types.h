82bcd77dfec51487526c7972c8c8c247ec621eb9
Jacob Feng <peanutchowder@gmail.com>
2025-10-26 16:58:23 -0600

    feat: updated promotion table for pt1 types

--- include/Types.h
+++ include/Types.h
@@ -7,8 +7,13 @@
 // Type enum for all nodes
 enum class ValueType {
     UNKNOWN,
+    BOOL,
+    CHARACTER,
     INTEGER,
-    VECTOR
+    VECTOR,
+    REAL,
+    TUPLE,
+    STRING
 };
 
 std::string toString(ValueType type);
@@ -18,18 +23,32 @@ inline ValueType promote(ValueType from, ValueType to) {
         case ValueType::UNKNOWN:
             return ValueType::UNKNOWN;
             break;
+        case ValueType::BOOL:
+            switch(to) {
+                case ValueType::BOOL:       return ValueType::BOOL;
+                case ValueType::VECTOR:     return ValueType::VECTOR; // implicit
+            }
+            break;
         case ValueType::INTEGER:
             switch (to) {
-                case ValueType::UNKNOWN: return ValueType::UNKNOWN;
-                case ValueType::INTEGER: return ValueType::INTEGER;
-                case ValueType::VECTOR:  return ValueType::VECTOR;
+                case ValueType::INTEGER:    return ValueType::INTEGER; 
+                case ValueType::VECTOR:     return ValueType::VECTOR;   // implicit
+                case ValueType::REAL:       return ValueType::REAL;
             }
             break;
         case ValueType::VECTOR:
             switch (to) {
-                case ValueType::UNKNOWN: return ValueType::UNKNOWN;
-                case ValueType::INTEGER: return ValueType::UNKNOWN;
-                case ValueType::VECTOR:  return ValueType::VECTOR;
+                case ValueType::VECTOR:     return ValueType::VECTOR;
+            }
+            break;
+        case ValueType::REAL:
+            switch (to) {
+                case ValueType::REAL:       return ValueType::REAL;
+            }
+            break;
+        case ValueType::TUPLE:
+            switch (to) {
+                case ValueType::TUPLE:      return ValueType::TUPLE;    // only if same len + internal types are convertible element-wise
             }
             break;
     }
