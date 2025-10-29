ee044f80a4640cbac27ce4e749750670386d106e
Jacob Feng <peanutchowder@gmail.com>
2025-10-28 22:36:50 -0600

    CHORE: removed unused legacy code

--- include/SemanticAnalysisVisitor.h
+++ include/SemanticAnalysisVisitor.h
@@ -10,28 +10,8 @@
 class SemanticAnalysisVisitor: public ASTVisitor {
     public:
         void visit(FileNode* node) override;
-        void visit(CondNode* node) override;
-        void visit(LoopNode* node) override;
-        void visit(IntDecNode* node) override;
-        void visit(VectorDecNode* node) override;
-        void visit(AssignNode* node) override;
 
-        // Values
-        void visit(IntNode* node) override;
-        void visit(IdNode* node) override;
-
-        // expressions 
-        void visit(BinaryOpNode* node) override;
-        void visit(RangeNode* node) override;
-        void visit(IndexNode* node) override;
-
-        // generators
-        void visit(GeneratorNode* node) override;
-        void visit(FilterNode* node) override; 
-        
-        void visit(PrintNode* node) override;
-
-        // New gazprea methods
+        // Operators
         void visit(UnaryExpr* node) override;   // unary+, unary-, not
         void visit(ExpExpr* node) override;     // ^
         void visit(MultExpr* node) override;    // *,/,%
