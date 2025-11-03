a0299e11f7f2795fc7c8b0da181f9e82b8737313
Jacob Feng <peanutchowder@gmail.com>
2025-11-03 12:32:37 -0700

    CHORE: fix indentation

--- include/MLIRgen.h
+++ include/MLIRgen.h
@@ -15,60 +15,60 @@ class MLIRGen : public ASTVisitor {
 public:
     explicit MLIRGen(BackEnd& backend);
 
-        void visit(FileNode* node) override;
+    void visit(FileNode* node) override;
 
-        // Functions
-        void visit(FuncStatNode* node) override; 
-        void visit(FuncPrototypeNode* node) override;
-        void visit(FuncBlockNode* node) override;
-        void visit(ProcedureNode* node) override;
-        
-        // Declarations
-        void visit(TypedDecNode* node) override;
-        void visit(InferredDecNode* node) override;
-        void visit(TupleTypedDecNode* node) override;
-        void visit(TypeAliasDecNode* node) override;
-        void visit(TypeAliasNode* node) override;
-        void visit(TupleTypeAliasNode* node) override;
+    // Functions
+    void visit(FuncStatNode* node) override; 
+    void visit(FuncPrototypeNode* node) override;
+    void visit(FuncBlockNode* node) override;
+    void visit(ProcedureNode* node) override;
+    
+    // Declarations
+    void visit(TypedDecNode* node) override;
+    void visit(InferredDecNode* node) override;
+    void visit(TupleTypedDecNode* node) override;
+    void visit(TypeAliasDecNode* node) override;
+    void visit(TypeAliasNode* node) override;
+    void visit(TupleTypeAliasNode* node) override;
 
-        // Statements
-        void visit(AssignStatNode* node)    override;
-        void visit(OutputStatNode* node)    override;
-        void visit(InputStatNode* node)     override;
-        void visit(BreakStatNode* node)     override;
-        void visit(ContinueStatNode* node)  override;
-        void visit(ReturnStatNode* node)    override;
-        void visit(CallStatNode* node)      override; 
-        void visit(IfNode* node)            override;
-        void visit(LoopNode* node)          override;
-        void visit(BlockNode* node)         override;
+    // Statements
+    void visit(AssignStatNode* node)    override;
+    void visit(OutputStatNode* node)    override;
+    void visit(InputStatNode* node)     override;
+    void visit(BreakStatNode* node)     override;
+    void visit(ContinueStatNode* node)  override;
+    void visit(ReturnStatNode* node)    override;
+    void visit(CallStatNode* node)      override; 
+    void visit(IfNode* node)            override;
+    void visit(LoopNode* node)          override;
+    void visit(BlockNode* node)         override;
 
 
-        // Expressions / Operators
-        void visit(ParenExpr* node) override;
-        void visit(UnaryExpr* node) override;   // unary+, unary-, not
-        void visit(ExpExpr* node) override;     // ^
-        void visit(MultExpr* node) override;    // *,/,%
-        void visit(AddExpr* node) override;     // +, -
-        void visit(CompExpr* node) override;    // <, >, <=, >=
-        void visit(NotExpr* node) override;     // not
-        void visit(EqExpr* node) override;      // ==, !=
-        void visit(AndExpr* node) override;     // and
-        void visit(OrExpr* node) override;      // or, xor
-        void visit(TupleAccessNode* node) override;
-        void visit(TypeCastNode* node) override;
-        void visit(TupleTypeCastNode* node) override;
+    // Expressions / Operators
+    void visit(ParenExpr* node) override;
+    void visit(UnaryExpr* node) override;   // unary+, unary-, not
+    void visit(ExpExpr* node) override;     // ^
+    void visit(MultExpr* node) override;    // *,/,%
+    void visit(AddExpr* node) override;     // +, -
+    void visit(CompExpr* node) override;    // <, >, <=, >=
+    void visit(NotExpr* node) override;     // not
+    void visit(EqExpr* node) override;      // ==, !=
+    void visit(AndExpr* node) override;     // and
+    void visit(OrExpr* node) override;      // or, xor
+    void visit(TupleAccessNode* node) override;
+    void visit(TypeCastNode* node) override;
+    void visit(TupleTypeCastNode* node) override;
 
-        // variables
-        void visit(IdNode* node) override;
+    // variables
+    void visit(IdNode* node) override;
 
-        // Primitives
-        void visit(TrueNode* node) override;
-        void visit(FalseNode* node) override;
-        void visit(CharNode* node) override;
-        void visit(IntNode* node) override;
-        void visit(RealNode* node) override;
-        void visit(TupleLiteralNode* node) override;
+    // Primitives
+    void visit(TrueNode* node) override;
+    void visit(FalseNode* node) override;
+    void visit(CharNode* node) override;
+    void visit(IntNode* node) override;
+    void visit(RealNode* node) override;
+    void visit(TupleLiteralNode* node) override;
 
 private:
     mlir::Value popValue();
