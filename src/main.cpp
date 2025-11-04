50a384f26f66705d02f934b9c0ac872bd2572efd
Chloe-lh <chloehaynes5252@gmail.com>
2025-11-04 16:09:29 -0700

    Revert changes to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,11 @@
 #include <fstream>
 
 int main(int argc, char **argv) {
-  // if (argc < 3) {
-  //   std::cout << "Missing required argument.\n"
-  //             << "Required arguments: <input file path> <output file path>\n";
-  //   return 1;
-  // }
-
-   const std::string src = R"(
-        dec var integer x = 1 END
-        dec var integer y = 2 END
-        x = x + y END
-    )";
-
-   
-   
+  if (argc < 3) {
+    std::cout << "Missing required argument.\n"
+              << "Required arguments: <input file path> <output file path>\n";
+    return 1;
+  }
 
   // Open the file then parse and lex it.
   antlr4::ANTLRFileStream afs;
@@ -38,22 +29,14 @@ int main(int argc, char **argv) {
   gazprea::GazpreaParser parser(&tokens);
 
   // Add our custom error listener for syntax errors
-  // parser.removeErrorListeners();
-  // parser.addErrorListener(new ErrorListener()); 
+  parser.removeErrorListeners();
+  parser.addErrorListener(new ErrorListener()); 
 
   // Get the root of the parse tree. Use your base rule name.
-  
-  auto fileCtx = parser.file();
+  auto *tree = parser.file();
   gazprea::ASTBuilder builder;
-  std::any anyAst = builder.visitFile(fileCtx);
-
-  assert(anyAst.has_value());
-  auto fileNode = std::any_cast<std::shared_ptr<ASTNode>>(anyAst);
-  assert(fileNode);
-
-    // Ensure it's a FileNode
-  auto fnode = std::dynamic_pointer_cast<FileNode>(fileNode);
-  assert(fnode);
+  std::any astAny = builder.visitFile(tree);
+  auto ast = std::any_cast<std::shared_ptr<FileNode>>(astAny);
 
   // HOW TO USE A VISITOR
   // Make the visitor
@@ -61,20 +44,11 @@ int main(int argc, char **argv) {
   // Visit the tree
   // visitor.visit(tree);
 
+  std::ofstream os(argv[2]);
+  BackEnd backend;
+  backend.emitModule();
+  backend.lowerDialects();
+  backend.dumpLLVM(os);
 
-  if (fnode->stats.empty()) {
-      std::cerr << "ASTBuilder produced empty file node\n";
-      return 2;
-  }
-
-  std::cout << "ASTBuilder smoke test passed: top-level node count = " << fnode->stats.size() << "\n";
   return 0;
-
-  // std::ofstream os(argv[2]);
-  // BackEnd backend;
-  // backend.emitModule();
-  // backend.lowerDialects();
-  // backend.dumpLLVM(os);
-
-  // return 0;
 }
